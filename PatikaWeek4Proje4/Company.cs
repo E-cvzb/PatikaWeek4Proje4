c47d8a3a1dc0d7fb497868488ed1613a813ed265
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-06 14:26:37 +0300

    Proje dosyası ekle.

---
+++ PatikaWeek4Proje4/Company.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4Proje4
+{
+    public abstract class Company // Soyut olarak olarak kişinin bilgileri tanımlanır 
+    {
+        string _name;
+        string _surname;
+        
+        string name {  get; set; }
+        string surname {  get; set; }
+        public virtual void Info()//kişinin bilgileri metot ile tanımlanır 
+        {
+            _name = name;
+            _surname = surname;
+
+           
+        }
+        public virtual void DutyInfo()//kişinin meslek bilgisi
+        {
+            Console.WriteLine("Lütfen ne iş yaptığınızı söyleyiniz");
+            Console.ReadLine();
+        }
+
+    }
+   public class Hasan : Company//hasan sınıfı company in alt sınıfı olmu oldu
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Hasan Çıldırmış");
+        }
+        public override void DutyInfo()
+        {
+            base.DutyInfo();// mesleğini ekrena yazdırma
+        }
+    }
+    public class Merve : Company
+    {
+        public override void Info()
+        {
+            Console.WriteLine("Merve Kara");
+        }
+        public override void DutyInfo()
+        {
+            base.DutyInfo();
+        }
+    }
+}
---
+++ PatikaWeek4Proje4/Program.cs
@@ -0,0 +1,13 @@
+﻿using PatikaWeek4Proje4;
+
+Company Hasan = new Hasan();
+
+Hasan.Info();
+Hasan.DutyInfo();
+Console.WriteLine("Teşekkürler");
+
+Company Merve = new Merve();
+
+Merve.Info();
+Merve.DutyInfo();
+Console.WriteLine("Teşekkürler");
